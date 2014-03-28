f0ed0dacc500c15d6685627f5b83c6f2c28124ed
dougellison <doug.ellison@gmail.com>
2014-03-27 22:09:06 -0600

    Added a bunch of logic for text

--- example/js/main.js
+++ example/js/main.js
@@ -38,7 +38,7 @@ function init() {
 	document.body.appendChild( container );
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.set( 0, 200, 800 );
+	camera.position.set( 0, 150	, 800 );
 
 	scene = new THREE.Scene();
 
@@ -61,96 +61,76 @@ function init() {
 	var line = new THREE.Line( geometry, line_material, THREE.LinePieces );
 	scene.add( line );
 
-	// Materials
-
-	var texture = new THREE.Texture( generateTexture() );
-	texture.needsUpdate = true;
-
-	materials.push( new THREE.MeshLambertMaterial( { map: texture, transparent: true } ) );
-	materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.FlatShading } ) );
-	materials.push( new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } ) );
-	materials.push( new THREE.MeshNormalMaterial( ) );
-	materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, transparent: true, blending: THREE.AdditiveBlending } ) );
-	//materials.push( new THREE.MeshBasicMaterial( { color: 0xff0000, blending: THREE.SubtractiveBlending } ) );
-
-	materials.push( new THREE.MeshLambertMaterial( { color: 0xdddddd, shading: THREE.SmoothShading } ) );
-	materials.push( new THREE.MeshPhongMaterial( { ambient: 0x030303, color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.SmoothShading, map: texture, transparent: true } ) );
-	materials.push( new THREE.MeshNormalMaterial( { shading: THREE.SmoothShading } ) );
-	materials.push( new THREE.MeshBasicMaterial( { color: 0xffaa00, wireframe: true } ) );
-
-	materials.push( new THREE.MeshDepthMaterial() );
-
-	materials.push( new THREE.MeshLambertMaterial( { color: 0x666666, emissive: 0xff0000, ambient: 0x000000, shading: THREE.SmoothShading } ) );
-	materials.push( new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0x666666, emissive: 0xff0000, ambient: 0x000000, shininess: 10, shading: THREE.SmoothShading, opacity: 0.9, transparent: true } ) );
-
-	materials.push( new THREE.MeshBasicMaterial( { map: texture, transparent: true } ) );
-
-	// Spheres geometry
-
-	var geometry_smooth = new THREE.SphereGeometry( 70, 32, 16 );
-	var geometry_flat = new THREE.SphereGeometry( 70, 32, 16 );
-	var geometry_pieces = new THREE.SphereGeometry( 70, 32, 16 ); // Extra geometry to be broken down for MeshFaceMaterial
-
-	for ( var i = 0, l = geometry_pieces.faces.length; i < l; i ++ ) {
-
-		var face = geometry_pieces.faces[ i ];
-		face.materialIndex = Math.floor( Math.random() * materials.length );
-
-	}
-
-	geometry_pieces.materials = materials;
-
-	materials.push( new THREE.MeshFaceMaterial( materials ) );
-
-	objects = [];
-
+	
 	var sphere, geometry, material;
+	var text = "three.js",
+	height = 20,
+	size = 70,
+	hover = 30,
 
-	for ( var i = 0, l = materials.length; i < l; i ++ ) {
+	curveSegments = 4,
 
-		material = materials[ i ];
+	bevelThickness = 2,
+	bevelSize = 1.5,
+	bevelSegments = 3,
+	bevelEnabled = true,
 
-		geometry = material instanceof THREE.MeshFaceMaterial ? geometry_pieces :
-				   ( material.shading == THREE.FlatShading ? geometry_flat : geometry_smooth );
+	font = "optimer", // helvetiker, optimer, gentilis, droid sans, droid serif
+	weight = "bold", // normal bold
+	style = "normal"; // normal italic
+	
+	
+	var textGeo = new THREE.TextGeometry("HELLO", {
+					size: size,
+					height: height,
+					curveSegments: curveSegments,
+					font: font,
+					weight: weight,
+					style: style,
+					bevelThickness: bevelThickness,
+					bevelSize: bevelSize,
+					bevelEnabled: bevelEnabled
+					});
+					
+	var material = new THREE.MeshFaceMaterial( [ 
+					new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading } ), // front
+					new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.SmoothShading } ) // side
+				] );					
+	textMesh1 = new THREE.Mesh( textGeo, material );
+	scene.add(textMesh1);
+	var geometry = new THREE.SphereGeometry( 50, 32, 32 );
+	var material = new THREE.MeshBasicMaterial( {color: 0x33ccff, wireframe: true} );
+	var sphere = new THREE.Mesh( geometry, material );
+	scene.add( sphere );
 
-		sphere = new THREE.Mesh( geometry, material );
-
-		sphere.position.x = ( i % 4 ) * 200 - 400;
-		sphere.position.z = Math.floor( i / 4 ) * 200 - 200;
-
-		sphere.rotation.x = Math.random() * 200 - 100;
-		sphere.rotation.y = Math.random() * 200 - 100;
-		sphere.rotation.z = Math.random() * 200 - 100;
-
-		objects.push( sphere );
-
-		scene.add( sphere );
-
-	}
-
-	particleLight = new THREE.Mesh( new THREE.SphereGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
-	scene.add( particleLight );
+	//particleLight = new THREE.Mesh( new THREE.SphereGeometry( 4, 8, 8 ), new THREE.MeshBasicMaterial( { color: 0xffffff } ) );
+	
+	//var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+	//pointLight.position.set( 0, 100, 90 );
+	//pointLight.color.setHSL( Math.random(), 1, 0.5 );
+	//scene.add(pointLight);
+	//scene.add( particleLight );
 
 	// Lights
 
 	scene.add( new THREE.AmbientLight( 0x111111 ) );
 
-	var directionalLight = new THREE.DirectionalLight( /*Math.random() * */ 0xffffff, 0.125 );
+	//var directionalLight = new THREE.DirectionalLight( /*Math.random() * */ 0xffffff, 0.125 );
 
-	directionalLight.position.x = Math.random() - 0.5;
-	directionalLight.position.y = Math.random() - 0.5;
-	directionalLight.position.z = Math.random() - 0.5;
+	//directionalLight.position.x = Math.random() - 0.5;
+	//directionalLight.position.y = Math.random() - 0.5;
+	//directionalLight.position.z = Math.random() - 0.5;
 
-	directionalLight.position.normalize();
+	//directionalLight.position.normalize();
 
-	scene.add( directionalLight );
+	//scene.add( directionalLight );
 
-	pointLight = new THREE.PointLight( 0xffffff, 1 );
-	scene.add( pointLight );
+	//pointLight = new THREE.PointLight( 0xffffff, 1 );
+	//scene.add( pointLight );
 
 	//
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
 	container.appendChild( renderer.domElement );
@@ -169,35 +149,6 @@ function onWindowResize() {
 
 }
 
-function generateTexture() {
-
-	var canvas = document.createElement( 'canvas' );
-	canvas.width = 256;
-	canvas.height = 256;
-
-	var context = canvas.getContext( '2d' );
-	var image = context.getImageData( 0, 0, 256, 256 );
-
-	var x = 0, y = 0;
-
-	for ( var i = 0, j = 0, l = image.data.length; i < l; i += 4, j ++ ) {
-
-		x = j % 256;
-		y = x == 0 ? y + 1 : y;
-
-		image.data[ i ] = 255;
-		image.data[ i + 1 ] = 255;
-		image.data[ i + 2 ] = 255;
-		image.data[ i + 3 ] = Math.floor( x ^ y );
-
-	}
-
-	context.putImageData( image, 0, 0 );
-
-	return canvas;
-
-}
-
 //
 
 function animate() {
@@ -212,32 +163,17 @@ function render() {
 
 	var timer = 0.0001 * Date.now();
 
-	//camera.position.x = Math.cos( timer ) * 1000;
-	//camera.position.z = Math.sin( timer ) * 1000;
-
 	camera.lookAt( scene.position );
 
-	for ( var i = 0, l = objects.length; i < l; i ++ ) {
+	//for ( var i = 0, l = objects.length; i < l; i ++ ) {
 
-		var object = objects[ i ];
+		//var object = objects[ i ];
 
-		object.rotation.x += rotateSize;
+		//object.rotation.x += rotateSize;
 //		object.rotation.y += 0.005;
-	}
-	if (rotateSize > 0)
-		rotateSize -= 0.001;
-
-//	materials[ materials.length - 3 ].emissive.setHSL( 0.54, 1, 0.35 * ( 0.5 + 0.5 * Math.sin( 35 * timer ) ) );
-//	materials[ materials.length - 4 ].emissive.setHSL( 0.04, 1, 0.35 * ( 0.5 + 0.5 * Math.cos( 35 * timer ) ) );
-
-//	particleLight.position.x = Math.sin( timer * 7 ) * 300;
-//	particleLight.position.y = Math.cos( timer * 5 ) * 400;
-//	particleLight.position.z = Math.cos( timer * 3 ) * 300;
-
-//	pointLight.position.x = particleLight.position.x;
-//	pointLight.position.y = particleLight.position.y;
-//	pointLight.position.z = particleLight.position.z;
-
+	//}
+	//if (rotateSize > 0)
+	//	rotateSize -= 0.001;
 	renderer.render( scene, camera );
 
 }
