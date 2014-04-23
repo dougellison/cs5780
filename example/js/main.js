6a983984fa5390b055c3b63162476464ef0f7c1e
dougellison <doug.ellison@gmail.com>
2014-04-23 09:46:04 -0600

    updated camera

--- example/js/main.js
+++ example/js/main.js
@@ -109,7 +109,7 @@ function init() {
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
 	//camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 1000 );
-	camera.position.set( 0, -20, 300 );
+	camera.position.set( 0, -80, 500 );
 	camera.lookAt(new THREE.Vector3(0,50,0));
 	camera.updateProjectionMatrix();
 	//camera.lookAt(new THREE.Vector3(0,0,0));
